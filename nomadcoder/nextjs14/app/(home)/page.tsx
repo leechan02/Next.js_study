b296f39ec1ab7523a532e8e35bb3d41b6d977530
Eui Lee <lechan1317@gmail.com>
2024-08-10 13:06:07 +0900

    feat: server side

--- nomadcoder/nextjs14/app/(home)/page.tsx
+++ nomadcoder/nextjs14/app/(home)/page.tsx
@@ -1,21 +1,14 @@
-'use client';
-import { useEffect, useState } from "react";
+export const metadata = {
+  title: "Home",
+};
 
-export default function Tomato() {
-  const [movies, setMovies] = useState();
-  const [isLoading, setIsLoading] = useState(true);
-  const getMovies = async () => {
-    const response = await fetch("https://nomad-movies.nomadcoders.workers.dev/movies");
-    const json = await response.json();
-    setMovies(json);
-    setIsLoading(false);
-  }
-  useEffect(() => {
-    getMovies();
-  }, []);
-  return (
-    <div>
-      <div>{isLoading ? "Loading..." : JSON.stringify(movies)}</div>
-    </div>
-  );
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+
+async function getMovies() {
+  return fetch(URL).then((response) => response.json());
+}
+
+export default async function HomePage() {
+  const movies = await getMovies();
+  return <div>{JSON.stringify(movies)}</div>;
 }
