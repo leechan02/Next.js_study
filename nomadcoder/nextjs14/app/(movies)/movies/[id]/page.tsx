51cfaae5478c801122aa35c36d020d157c419237
Eui Lee <lechan1317@gmail.com>
2024-08-10 13:35:51 +0900

    feat: parallel request

--- nomadcoder/nextjs14/app/(movies)/movies/[id]/page.tsx
+++ nomadcoder/nextjs14/app/(movies)/movies/[id]/page.tsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { API_URL } from "../../../(home)/page";
 
-export default function MovieDetail({
+async function getMovie(id: string) {
+  return fetch(`${API_URL}/${id}`).then((response) => response.json());
+}
+
+async function getVideos(id: string) {
+  return fetch(`${API_URL}/${id}/videos`).then((response) => response.json());
+}
+
+export default async function MovieDetail({
   params: { id },
 }: {
   params: { id: string };
 }) {
-  return <h1>Movie {id}</h1>;
+  const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
+  return <h1>{movie.title}</h1>;
 }
