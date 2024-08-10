96c22350429990cf08c4d4b43dbcf83be1b21066
Eui Lee <lechan1317@gmail.com>
2024-08-10 10:36:53 +0900

    feat: metadata

--- nomadcoder/nextjs14/app/not-found.tsx
+++ nomadcoder/nextjs14/app/not-found.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 import Navigation from "../components/navigation";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Not Found",
+};
 
 export default function NotFound() {
   return (
