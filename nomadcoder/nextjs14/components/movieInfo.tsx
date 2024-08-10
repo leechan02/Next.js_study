9c4cd274d98ca0e0f08ec4c7c66545476c6668c6
Eui Lee <lechan1317@gmail.com>
2024-08-10 14:41:03 +0900

    feat: movie trailers

--- nomadcoder/nextjs14/components/movieInfo.tsx
+++ nomadcoder/nextjs14/components/movieInfo.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { API_URL } from "../app/(home)/page";
+import styles from "../styles/movieInfo.module.css"
 
 async function getMovie(id: string) {
   return fetch(`${API_URL}/${id}`).then((response) => response.json());
@@ -7,5 +8,15 @@ async function getMovie(id: string) {
 
 export default async function MovieInfo({ id }: { id: string }) {
   const movie = await getMovie(id);
-  return <h6>{JSON.stringify(movie)}</h6>;
+  return (
+    <div className={styles.container}>
+      <img src={movie.poster_path} alt={movie.title} className={styles.poster}/>
+      <div className={styles.info}>
+        <h1 className={styles.title}>{movie.title}</h1>
+        <h3>⭐️ {movie.vote_average.toFixed(1)}</h3>
+        <p>{movie.overview}</p>
+        <a href={movie.homepage} target={"_blank"}>Homepage &rarr;</a>
+      </div>
+    </div>
+  );
 }
--- nomadcoder/nextjs14/components/movieVideos.tsx
+++ nomadcoder/nextjs14/components/movieVideos.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { API_URL } from "../app/(home)/page";
+import styles from "../styles/movieVideos.module.css";
 
 async function getVideos(id: string) {
   return fetch(`${API_URL}/${id}/videos`).then((response) => response.json());
@@ -7,5 +8,17 @@ async function getVideos(id: string) {
 
 export default async function MovieVideos({ id }: { id: string }) {
   const videos = await getVideos(id);
-  return <h6>{JSON.stringify(videos)}</h6>;
+  return (
+    <div className={styles.container}>
+      {videos.map((video) => (
+        <iframe
+          key={video.id}
+          src={`https://youtube.com/embed/${video.key}`}
+          allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+          allowFullScreen
+          title={video.name}
+        />
+      ))}
+    </div>
+  );
 }
